2f3a0dff65d568c255ae75f127a69dacdec193b0
AyushSN <ayushsanjivnaidu@gmail.com>
2019-05-15 14:40:45 +0530

    minor refactoring

--- events/message.js
+++ events/message.js
@@ -1,5 +1,7 @@
+const ping = require('../commands/ping');
+
 module.exports = (client, message) => {
     if (message.content.startsWith('!ping')) {
-        return message.channel.send('Pong!')
+        return ping(message);
     }
 }
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
-const Discord = require('discord.js')
-const fs = require('fs')
-const client = new Discord.Client()
+require('dotenv').config();
+const Discord = require('discord.js');
+const fs = require('fs');
+const client = new Discord.Client();
 fs.readdir('./events/', (err, files) => {
   files.forEach(file => {
-    const eventHandler = require(`./events/${file}`)
-    const eventName = file.split('.')[0]
-    client.on(eventName, arg => eventHandler(client, arg))
+    const eventHandler = require(`./events/${file}`);
+    const eventName = file.split('.')[0];
+    client.on(eventName, arg => eventHandler(client, arg));
   })
 })
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
